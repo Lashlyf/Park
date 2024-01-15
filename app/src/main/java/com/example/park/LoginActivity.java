693578f4228f3463f7a87e6ad20255b64684c8f5
Lashlyf <ladeshamitchelle@gmail.com>
2024-01-15 10:07:57 +0300

    Added changes

--- app/src/main/java/com/example/park/LoginActivity.java
+++ app/src/main/java/com/example/park/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.park;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        AppCompatButton Login = findViewById(R.id.login);
+        Login.setOnClickListener(view ->{
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
