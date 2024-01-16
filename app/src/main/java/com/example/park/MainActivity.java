6d53f7e669e6f2e031c911b8e1ee35e25cd356b9
Lashlyf <ladeshamitchelle@gmail.com>
2024-01-16 15:19:59 +0300

    Recent changes

--- app/src/main/java/com/example/park/MainActivity.java
+++ app/src/main/java/com/example/park/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.park;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView Booking = findViewById(R.id.Booking);
+        TextView  Book = findViewById(R.id.text6)
+
+        Booking.setOnClickListener(View ->{
+            Intent intent = new Intent(MainActivity.this, MyBookingActivity.class);
+            startActivity(intent);
+        });
+
+        Book.setOnClickListener(View ->{
+            Intent intent = new Intent(MainActivity.this, MyBookingActivity.class);
+            startActivity(intent);
+        });
     }
 }
